4e974a53f61d143f173224b3b34e86e068a93846
丁韶峰 <thudsf19@gmail.com>
2021-08-19 21:56:30 +0800

    user system framework

---
+++ src/main/java/com/example/demo/api/UserController.java
@@ -0,0 +1,33 @@
+package com.example.demo.api;
+
+import com.example.demo.service.UserService;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@RequestMapping("/accounts")
+@Controller
+public class UserController {
+  private static UserService userService;
+
+  @ResponseBody
+  @GetMapping
+  public void login() {
+    ;
+  }
+
+  @ResponseBody
+  @PostMapping
+  public void register() {
+    ;
+  }
+
+  @ResponseBody
+  @PostMapping("/changepassword")
+  public void changePassword() {
+    ;
+  }
+}
