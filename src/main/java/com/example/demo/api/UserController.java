e2acca54e23b58c608464b0532879af9f59a2c7f
丁韶峰 <thudsf19@gmail.com>
2021-08-21 13:46:29 +0800

    api for login

--- src/main/java/com/example/demo/api/UserController.java
+++ src/main/java/com/example/demo/api/UserController.java
@@ -1,22 +1,61 @@
 package com.example.demo.api;
 
-import com.example.demo.service.UserService;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+import java.util.regex.Pattern;
 
+import com.example.demo.model.User;
+import com.example.demo.service.UserService;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ObjectNode;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @RequestMapping("/accounts")
 @Controller
 public class UserController {
-  private static UserService userService;
+  private final UserService userService;
+
+  UserController(UserService userService) {
+    this.userService = userService;
+  }
+  @Autowired
+  private ObjectMapper mapper;
 
   @ResponseBody
   @GetMapping
-  public String login() {
-    return "Hello, World";
+  @ResponseStatus(HttpStatus.BAD_GATEWAY)
+  public ObjectNode login(@RequestParam String nameOrEmail, @RequestParam("hashedPassword") String hashedPassword) {
+    ObjectNode json = mapper.createObjectNode();
+    final String namePattern = "^([a-zA-Z0-9_-]{1,63})$";
+    final String pwdPattern = "[A-Fa-f0-9]{64}";
+    if (Pattern.matches(namePattern, hashedPassword) != true) {
+      json.put("success", false);
+      json.put("message", "invalid name");
+    }
+    if (Pattern.matches(pwdPattern, hashedPassword) != true) {
+      json.put("success", false);
+      json.put("message", "invalid hashedPassword");
+    }
+    User user = userService.getUser(nameOrEmail, hashedPassword, 1);
+    if (user == null) {
+      json.put("success", false);
+      json.put("message", "wrong name or password");
+    }
+    json.put("id", user.getId());
+    json.put("name", user.getName());
+    json.put("email", user.getEmail());
+    return json;
   }
 
   @ResponseBody
--- src/main/java/com/example/demo/service/UserService.java
+++ src/main/java/com/example/demo/service/UserService.java
@@ -1,5 +1,36 @@
 package com.example.demo.service;
 
+import com.example.demo.dao.UserDao;
+import com.example.demo.model.User;
+
+// enum UserOpStatus {
+//   SUCCESS,
+//   INVALID_NAME,
+//   INVALID_EMAIL,
+//   INVALID_PWD,
+//   WRONG_NAME,
+//   WRONG_EMAIL,
+//   WRONG_PWD
+// }
+
+
 public class UserService {
-  
+  private final UserDao userDao;
+  UserService(UserDao userDao) {
+    this.userDao = userDao;
+  }
+  public User getUser(String nameOrEmail, String pwd, int mode) {
+    /** 
+    /* @param 
+      
+    */
+    return new User();
+  }
+  public boolean createUser(String name, String Email, String pwd) {
+    return true;
+  }
+  public boolean changepassword(int id, String pwd) {
+    return true;
+  }
+
 }
