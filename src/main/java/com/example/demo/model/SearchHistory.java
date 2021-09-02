64ee25ded051f6cf1f70c5e30da2a28f9e53b995
丁韶峰 <thudsf19@gmail.com>
2021-09-02 17:43:53 +0800

    search history table definition

--- src/main/java/com/example/demo/model/SearchHistory.java
+++ src/main/java/com/example/demo/model/SearchHistory.java
@@ -2,6 +2,8 @@ package com.example.demo.model;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
@@ -9,6 +11,34 @@ import javax.persistence.Table;
 @Table(name = "SearchHistory")
 public class SearchHistory {
   @Id
+  @GeneratedValue(strategy = GenerationType.AUTO)
   @Column(name = "search_id", nullable = false)
   private Integer searchId;
+
+  @Column(name = "search_content")
+  private String searchContent;
+
+  public SearchHistory() {
+
+  }
+
+  public SearchHistory(String searchContent) {
+    this.searchContent = searchContent;
+  }
+
+  public Integer getId() {
+    return this.searchId;
+  }
+
+  public void setId(Integer searchId) {
+    this.searchId = searchId;
+  }
+
+  public String getContent() {
+    return this.searchContent;
+  }
+
+  public void setContent(String searchContent) {
+    this.searchContent = searchContent;
+  }
 }
