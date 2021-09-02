be706fac3b3abeea7cb1f553b90c9c936823fa8b
丁韶峰 <thudsf19@gmail.com>
2021-09-02 14:40:27 +0800

    edu table definition

--- src/main/java/com/example/demo/model/Edu.java
+++ src/main/java/com/example/demo/model/Edu.java
@@ -8,16 +8,60 @@ import javax.persistence.Id;
 import javax.persistence.Table;
 
 @Entity
-@Table(name = "Record")
+@Table(name = "Edu")
 public class Edu {
   @Id
   @GeneratedValue(strategy = GenerationType.AUTO)
   private Integer id;
 
-  @Column(name = "course")
+  @Column(name = "course", nullable = false)
   private String course;
 
-  @Column(name = "entity")
-  private String entity;
+  @Column(name = "entity_name", nullable = false)
+  private String entity_name;
 
+  @Column(name = "uri", nullable = true)
+  private String uri; // uri for entity, id for exercise
+
+  public Edu() {
+
+  }
+
+  public Edu(String course, String entity_name, String uri) {
+    this.course = course;
+    this.entity_name = entity_name;
+    this.uri = uri;
+  }
+
+  public Integer getId() {
+    return this.id;
+  }
+
+  public String getCourse() {
+    return this.course;
+  }
+
+  public String getName() {
+    return this.entity_name;
+  }
+
+  public String getUri() {
+    return this.uri;
+  }
+
+  public void setId(Integer id) {
+    this.id = id;
+  }
+
+  public void setCourse(String course) {
+    this.course = course;
+  }
+
+  public void setName(String name) {
+    this.entity_name = name;
+  }
+
+  public void setUri(String uri) {
+    this.uri = uri;
+  }
 }
