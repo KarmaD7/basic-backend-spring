667a18139ef19c9582d3442542445c83ec8aafc1
丁韶峰 <thudsf19@gmail.com>
2021-09-03 14:58:40 +0800

    search history api

--- src/main/java/com/example/demo/api/HistoryController.java
+++ src/main/java/com/example/demo/api/HistoryController.java
@@ -1,9 +1,19 @@
 package com.example.demo.api;
 
-import javax.servlet.http.HttpServletResponse;
+import java.util.List;
 
+import javax.servlet.http.HttpServletResponse;
+import javax.swing.text.StyledEditorKit.BoldAction;
+
+import com.example.demo.service.HistoryService;
+import com.example.demo.utils.BadRequest;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ArrayNode;
 import com.fasterxml.jackson.databind.node.ObjectNode;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -11,10 +21,14 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
 
 @RequestMapping("history")
 @Controller
 public class HistoryController {
+  @Autowired
+  HistoryService historyService;
+
   @GetMapping("visit")
   @ResponseBody
   public String getVisitHistoryForUser(@RequestParam("id") String id) {
@@ -33,15 +47,40 @@ public class HistoryController {
     return null;
   }
 
-  @GetMapping("search")
+  @GetMapping("search/save")
   @ResponseBody
-  public String getSearchHistoryForUser(HttpServletResponse response, @RequestBody ObjectNode request, @RequestParam("id") String id) {
-    return null;
+  @ResponseStatus(HttpStatus.OK)
+  public ObjectNode getSearchHistoryForUser(@RequestParam("id") String id) {
+    ObjectNode json = new ObjectMapper().createObjectNode();
+    // int uid;
+    // try {
+    //   uid = Integer.parseInt(id);
+    // } catch (NumberFormatException e) {
+      
+    // } 
+    // cookie auth guarantees the id is valid.
+    List<String> history = historyService.getSearchHistory(Integer.parseInt(id));  
+    json.put("success", true);
+    ArrayNode arrayNode = json.putArray("history");
+    for (String item: history) {
+      arrayNode.add(item);
+    }
+    
+    return json;
   }
 
-  @PostMapping("search")
+  @GetMapping("search/get")
   @ResponseBody
   public String putSearchHistory(HttpServletResponse response, @RequestBody ObjectNode request, @RequestParam("id") String id) {
-    return null;
+    ObjectNode json = new ObjectMapper().createObjectNode();
+    JsonNode _key = request.get("key");
+    if (_key == null) {
+      response.setStatus(400);
+      return BadRequest.badRequest;
+    }
+    String key = _key.asText();
+    boolean success = historyService.addSearchHistory(Integer.parseInt(id), key);
+    json.put("success", success);
+    return json.asText();
   }
 }
