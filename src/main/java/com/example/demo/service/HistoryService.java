5114d1d4d044164fa38157b807a408e86ae0f15b
丁韶峰 <thudsf19@gmail.com>
2021-09-03 12:09:54 +0800

    search history service define

--- src/main/java/com/example/demo/service/HistoryService.java
+++ src/main/java/com/example/demo/service/HistoryService.java
@@ -1,5 +1,27 @@
 package com.example.demo.service;
 
+import java.util.List;
+
+import com.example.demo.dao.EduRepository;
+import com.example.demo.dao.SearchHistoryRepository;
+import com.example.demo.model.EduEntity;
+import com.example.demo.model.SearchHistory;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+
 public class HistoryService {
-  
+  @Autowired
+  SearchHistoryRepository historyDao;
+
+  @Autowired
+  EduRepository eduDao;
+
+  public List<String> getSearchHistory(Integer uid) {
+    return null;
+  }
+
+  public boolean addSearchHistory(Integer uid, String content) {
+    return true;
+  }
 }
