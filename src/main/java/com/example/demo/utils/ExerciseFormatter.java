3b53c5078509ed652563cc7937d88f6cd9e4e286
丁韶峰 <thudsf19@gmail.com>
2021-09-07 16:35:22 +0800

    exercise formatter

--- src/main/java/com/example/demo/utils/ExerciseFormatter.java
+++ src/main/java/com/example/demo/utils/ExerciseFormatter.java
@@ -1,5 +1,23 @@
 package com.example.demo.utils;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ObjectNode;
+
 public class ExerciseFormatter {
-  
+  public static ObjectNode getFormattedChoiceExercise(String content) {
+    Pattern pattern = Pattern.compile("^(.*)A.(.*)B.(.*)C.(.*)D.(.*)$");
+    Matcher matcher = pattern.matcher(content);
+    if (matcher.find() == true && matcher.groupCount() == 5) {
+      ObjectNode json = new ObjectMapper().createObjectNode();
+      json.put("question", matcher.group(1));
+      json.put("A", matcher.group(2));
+      json.put("B", matcher.group(3));
+      json.put("C", matcher.group(4));
+      json.put("D", matcher.group(5));
+      return json;
+    } else return null;
+  }
 }
