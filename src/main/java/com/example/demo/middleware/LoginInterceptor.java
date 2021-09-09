6b59f20c5851a7b74e054330e75ca5264514cc9e
丁韶峰 <thudsf19@gmail.com>
2021-09-09 14:26:59 +0800

    logger

--- src/main/java/com/example/demo/middleware/LoginInterceptor.java
+++ src/main/java/com/example/demo/middleware/LoginInterceptor.java
@@ -1,6 +1,8 @@
 package com.example.demo.middleware;
 
 import java.io.PrintWriter;
+import java.util.Arrays;
+import java.util.Map;
 
 import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
@@ -9,18 +11,39 @@ import javax.servlet.http.HttpServletResponse;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.node.ObjectNode;
 import com.google.common.base.Charsets;
+import com.google.common.collect.Multiset.Entry;
 import com.google.common.hash.Hashing;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.HandlerInterceptor;
 
 @Component
 public class LoginInterceptor implements HandlerInterceptor {
+
+  private static final Logger LOGGER = LoggerFactory.getLogger(LoginInterceptor.class);
+
   @Override
   public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
     response.setHeader("Access-Control-Allow-Origin", "*");
     response.setHeader("Access-Control-Allow-Headers", "Content-Type");
     response.setHeader("Content-Type", "application/json; charset=utf-8");
+    LOGGER.info("url : {}", request.getRequestURL());
+    // method
+    LOGGER.info("method : {}", request.getMethod());
+    // ip
+    LOGGER.info("ip : {}", request.getRemoteAddr());
+    
+    LOGGER.info("params");
+
+    Map<String, String[]> paramMap = request.getParameterMap();
+
+    for (Map.Entry<String, String[]> item: request.getParameterMap().entrySet()) {
+      LOGGER.info("{} : {}", item.getKey(), item.getValue());
+    }
+    LOGGER.info("params : {}", Arrays.asList(joinP));
+
     if (request.getRequestURI().equals("/account") || request.getRequestURI().equals("/edu")) {
       return true;
     }
